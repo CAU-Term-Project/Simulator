8a5b9a35de86c2f202a042b145585b759a040198
dk8660 <dk8660@naver.com>
2022-05-30 19:17:02 +0900

    2022-05-30 19:16 main 명령어 입력 정상 작동

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 #include "defines.h"
+#include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
 
 unsigned int PC, IR;
@@ -47,13 +48,30 @@ void main()
     
     while (1) {
         // Get command line
-        char* extendedCommand;
-        char command;
+        char command[2];
         printf("command: ");
-        scanf("%s", &extendedCommand);
-        command = extendedCommand[0];
-        switch (command) {
-        case 'l':
+        scanf("%s", command);
+        getchar();
+
+        if (strcmp(command, "sr") == 0) {
+            unsigned int registerNumber, value;
+            printf("Register Number: ");
+            scanf("%d", &registerNumber);
+            printf("Value: ");
+            scanf("%d", &value);
+            printf("R[%d] = %d\n", registerNumber, value);
+            REG(registerNumber, value, 1);
+        }
+        else if (strcmp(command, "sm") == 0) {
+            unsigned int location, value;
+            printf("Location: 0x");
+            scanf("%x", &location);
+            printf("Value: ");
+            scanf("%d", &value);
+            printf("MEM[%.8x] = %d\n", location, value);
+            MEM(location, value, 1, 2);
+        }
+        else if (command[0] == 'l') {
             resetMem();
 
             char* fileName;
@@ -91,24 +109,18 @@ void main()
                 addr += 4;
             }
             setPC(PROG_START);
-            
-            fclose(fp);
 
-            break;
-        case 'j':
+            fclose(fp);
+        }
+        else if (command[0] == 'j') {
             unsigned int jaddr;
             printf("Enter Jump Address: ");
             scanf("%d", &jaddr);
             setPC(jaddr);
-
-            break;
-        case 'g':
-            while (inst.RI.opc != SYSCALL) step();
-            break;
-        case 's':
-            step();
-            break;
-        case 'm':
+        }
+        else if(command[0] == 'g') while (inst.RI.opc != SYSCALL) step();
+        else if(command[0] == 's') step();
+        else if (command[0] == 'm') {
             unsigned int start, end;
             printf("Start: ");
             scanf("%d", &start);
@@ -118,31 +130,8 @@ void main()
             for (unsigned int addr = start; addr <= end; addr += 4) {
                 printf("MEM[%d]: 0x%.8x\n", addr, MEM(addr, 0, 0, 2));
             }
-            break;
-        case'r':
-            showRegister();
-            break;
-        case'x':
-            return;
-        default:
-            if (extendedCommand == "sr") {
-                unsigned int registerNumber, value;
-                printf("Register Number: ");
-                scanf("%d", &registerNumber);
-                printf("Value: ");
-                scanf("%d", &value);
-                printf("R[%d] = %d\n", registerNumber, value);
-                REG(registerNumber, value, 1);
-            }
-            else if (extendedCommand == "sm") {
-                unsigned int location, value;
-                printf("Location: 0x");
-                scanf("%x", &location);
-                printf("Value: ");
-                scanf("%d", &value);
-                printf("MEM[%.8x] = %d\n", location, value);
-                MEM(location, value, 1, 2);
-            }
         }
+        else if(command[0] =='r') showRegister();
+        else if(command[0] =='x') return;
     }
 }
